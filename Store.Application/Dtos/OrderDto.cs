486773270f53119c8b6e55752215a33523ed5441
CaioSNery <caionery40@gmail.com>
2025-08-23 14:53:20 -0300

    create:Dtos

---
+++ Store.Application/Dtos/OrderDto.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Application.Dtos
+{
+    public class OrderDto
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; }
+        public string CustomerName { get; set; }
+        public Guid CustomerId { get; set; }
+
+    }
+}
