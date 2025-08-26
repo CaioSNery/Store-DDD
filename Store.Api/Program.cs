cef2b7a6eda253d96fbd908d800d789b66658ba2
CaioSNery <caionery40@gmail.com>
2025-08-26 17:06:31 -0300

    Config swagger

--- Store.Api/Program.cs
+++ Store.Api/Program.cs
@@ -53,14 +53,19 @@ builder.Services.AddScoped<IHandler<DeleteOrderCommand>, OrderHandler>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "DeliveryFast API v1");
+    c.RoutePrefix = string.Empty; // abre direto na raiz
+});
+
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseRouting();
 
 app.MapControllers();
 
